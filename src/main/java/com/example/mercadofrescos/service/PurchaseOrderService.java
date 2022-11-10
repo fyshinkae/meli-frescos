4e32ea66f6ace6a880529240116632f9c639e27d
matheusaralves <matheus.dearaujo@mercadolivre.com>
2022-11-10 11:42:29 -0300

    Feat: Initial Order Methods

--- src/main/java/com/example/mercadofrescos/service/PurchaseOrderService.java
+++ src/main/java/com/example/mercadofrescos/service/PurchaseOrderService.java
@@ -1,33 +1,57 @@
 package com.example.mercadofrescos.service;
 
-import com.example.mercadofrescos.model.InboundOrder;
-import com.example.mercadofrescos.model.Product;
-import com.example.mercadofrescos.model.PurchaseItem;
-import com.example.mercadofrescos.model.PurchaseOrder;
+import com.example.mercadofrescos.dto.BatchStockDTO;
+import com.example.mercadofrescos.model.*;
+import com.example.mercadofrescos.repository.IBatchStockRepo;
+import com.example.mercadofrescos.repository.IProductRepo;
 import com.example.mercadofrescos.repository.IPurchaseOrderRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PurchaseOrderService {
 
-    private final IPurchaseOrderRepo repo;
+    private final IBatchStockRepo repo;
 
     public Double getPurchaseItems(PurchaseOrder purchaseOrder) {
 
 
     }
 
-    public Double getTotalPrice(InboundOrder inboundOrder) {
-        double totalPrice = 0;
+    public Double getCartAmount(<List<PurchaseItem> purchaseItemList) {
+        int availableQuantity = 0;
+        double totalCartAmount = 0d;
+        BigDecimal singleCartAmount;
+
+        for (PurchaseItem item : purchaseItemList) {
+
+            Product productId = item.getProductId();
+            Optional<BatchStock> product = repo.findById(item.getId());
+            if (product.isPresent()) {
+                BatchStock product1 = product.get();
+                Product productPrice = product.get().getProduct();
+                if (product1.getProductQuantity() < item.getProductQuantity()) {
+                   singleCartAmount = productPrice.getPrice().multiply(BigDecimal.valueOf(product1.getProductQuantity()));
+                    item.setProductQuantity(product1.getProductQuantity());
+                } else {
+                    singleCartAmount = productPrice.getPrice().multiply(BigDecimal.valueOf(item.getProductQuantity()));
+                    availableQuantity = product1.getProductQuantity() - item.getProductQuantity();
+                }
+                // totalCartAmount = total
+                        // totalCartAmount + singleCartAmount;
+            }
+        }
 
 
 
 
-        return totalPrice;
+
     }
 
 
--- src/main/java/com/example/mercadofrescos/service/interfaces/IBatchStockService.java
+++ src/main/java/com/example/mercadofrescos/service/interfaces/IBatchStockService.java
@@ -4,13 +4,16 @@ import com.example.mercadofrescos.dto.BatchStockDTO;
 import com.example.mercadofrescos.dto.InsertBatchRequestDTO;
 import com.example.mercadofrescos.model.BatchStock;
 import com.example.mercadofrescos.model.InboundOrder;
+import com.example.mercadofrescos.model.PurchaseItem;
 import com.example.mercadofrescos.model.Section;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IBatchStockService {
     BatchStockDTO save(InsertBatchRequestDTO request);
     BatchStockDTO update(InsertBatchRequestDTO request);
 
     List<BatchStock> validBatchStockList(InboundOrder inboundOrder);
+
 }
