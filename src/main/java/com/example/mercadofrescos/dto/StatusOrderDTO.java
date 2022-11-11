0e478b50d6adb17d4e64ab042864a190b080f77d
Giovanna Eliz <giovanna.eliz@mercadolivre.com>
2022-11-11 17:04:27 -0300

    Feat: updateOrderStatus is finished

---
+++ src/main/java/com/example/mercadofrescos/dto/StatusOrderDTO.java
@@ -0,0 +1,11 @@
+package com.example.mercadofrescos.dto;
+
+import com.example.mercadofrescos.model.enums.StatusOrder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class StatusOrderDTO {
+    private StatusOrder orderStatus;
+}
