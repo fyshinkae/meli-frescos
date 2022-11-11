fd5398ff6b2602e7385bb16c4567a301b46ba4f4
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-11-11 17:52:51 -0300

    refact: Malformatted JSON exceptionHandler has a generic message

--- src/main/java/com/example/mercadofrescos/dto/InsertBatchRequestDTO.java
+++ src/main/java/com/example/mercadofrescos/dto/InsertBatchRequestDTO.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.stream.Collectors;
 
