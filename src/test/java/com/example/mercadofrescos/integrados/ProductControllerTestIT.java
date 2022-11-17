ed3c74eaff52a966f7f725a9be5effd5e1d1e14e
fyshinkae <felipe.shinkae@mercadolivre.com>
2022-11-17 17:06:19 -0400

    Feat: finish integration test by product controller

--- src/test/java/com/example/mercadofrescos/integrados/ProductControllerTestIT.java
+++ src/test/java/com/example/mercadofrescos/integrados/ProductControllerTestIT.java
@@ -1,10 +1,13 @@
 package com.example.mercadofrescos.integrados;
 
+import com.example.mercadofrescos.dto.ProductAgentResponseDTO;
 import com.example.mercadofrescos.dto.ProductDTO;
 import com.example.mercadofrescos.dto.ProductResponseDTO;
 import com.example.mercadofrescos.mocks.ProductMock;
+import com.example.mercadofrescos.model.BatchStock;
 import com.example.mercadofrescos.model.Product;
 import com.example.mercadofrescos.model.enums.Category;
+import com.example.mercadofrescos.repository.IBatchStockRepo;
 import com.example.mercadofrescos.repository.IProductRepo;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +18,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,9 +28,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class ProductControllerTestIT  {
     @Autowired
     private MockMvc mockMvc;
@@ -33,19 +41,8 @@ public class ProductControllerTestIT  {
     @Autowired
     private IProductRepo productRepo;
 
-    @BeforeEach
-    void setup() {
-        Product product1 = new Product();
-        product1.setId(ProductMock.productTest().getId());
-        product1.setSeller(ProductMock.productTest().getSeller());
-        product1.setBatches(ProductMock.productTest().getBatches());
-        product1.setName(ProductMock.productTest().getName());
-        product1.setPrice(ProductMock.productTest().getPrice());
-        product1.setCategory(ProductMock.productTest().getCategory());
-
-        List<Product> productList = new ArrayList<>();
-        productList.add(product1);
-    }
+    @Autowired
+    private IBatchStockRepo batchRepo;
 
     @Test
     void getAll_retunsAllProducts_whenSuccess() throws Exception {
@@ -82,15 +79,50 @@ public class ProductControllerTestIT  {
     @Test
     void getAllProducts_returnsAllProducts_whenSuccess() throws Exception {
         Optional<Product> productResponse = productRepo.findById(1L);
-        System.out.println("Ola To aqui" + productResponse);
-
+        BatchStock batchesResponse = productResponse.get().getBatches().iterator().next();
 
         ResultActions response = mockMvc.perform(
-                get("/api/v1/fresh-products/list?productId=1&order=V", productResponse)
+                get("/api/v1/fresh-products/agent/list?productId=1&order=Q")
                         .contentType(MediaType.APPLICATION_JSON) );
 
         response.andExpect(status().isOk())
-                .andExpect(jsonPath("$").isNotEmpty());
+                .andExpect(jsonPath("$.batchStock.size()", CoreMatchers.is(productResponse.get().getBatches().size())))
+//                .andExpect(jsonPath("$.batchStock[0].batchNumber", CoreMatchers.is(1)))
+//                .andExpect(jsonPath("$.batchStock[0].currentQuantity", CoreMatchers.is(batchesResponse.getProductQuantity())))
+//                .andExpect(jsonPath("$.batchStock[0].dueDate", CoreMatchers.is(batchesResponse.getDueDate())))
+        ;
+    }
+
+    @Test
+    void getAllByCategory_returnException_whenHaveError() throws Exception {
+        ResultActions response = mockMvc.perform(
+                get("/api/v1/fresh-products/list?category=FROZEN")
+                        .contentType(MediaType.APPLICATION_JSON) );
+
+        response.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", CoreMatchers.is("No products with this category were found")));
+    }
+
+    @Test
+    void getAllByCategory_returnException_whenNoHaveProductId() throws Exception {
+        ResultActions response = mockMvc.perform(
+                get("/api/v1/fresh-products/agent/list?productId=1&order=XABLAU")
+                        .contentType(MediaType.APPLICATION_JSON) );
+
+        response.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", CoreMatchers.is("Invalid sort parameter")));
+        ;
+    }
+
+    @Test
+    void getAllByCategory_returnException_whenNoHaveProduct() throws Exception {
+        ResultActions response = mockMvc.perform(
+                get("/api/v1/fresh-products/agent/list?productId=20")
+                        .contentType(MediaType.APPLICATION_JSON) );
+
+        response.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", CoreMatchers.is("Product not found")));
+        ;
     }
 
 }
