3620428ccce3fe8726c52519b8da213b016baf6b
Anderson Alves <anderson.malves@mercadolivre.com>
2022-11-21 18:04:53 -0300

    Refactor: response of recurrence

--- src/main/java/com/example/mercadofrescos/dto/purchase/RecurrenceResponseDTO.java
+++ src/main/java/com/example/mercadofrescos/dto/purchase/RecurrenceResponseDTO.java
@@ -1,27 +1,22 @@
 package com.example.mercadofrescos.dto.purchase;
 
-import com.example.mercadofrescos.dto.batchStock.BatchStockDTO;
-import com.example.mercadofrescos.model.BatchStock;
-import com.example.mercadofrescos.model.PurchaseOrder;
 import com.example.mercadofrescos.model.RecurrenceOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecurrenceResponseDTO {
     private LocalDate nextPurchase;
-    // private PurchaseOrder purchaseOrder;
+    private PurchaseRequestDTO purchaseOrder;
 
 
-    public RecurrenceResponseDTO(RecurrenceOrder order) {
-        this.nextPurchase = order.getNextPurchase();
-        // this.purchaseOrder = order.getPurchaseOrder();
+    public RecurrenceResponseDTO(RecurrenceOrder recurrenceOrder) {
+        this.nextPurchase = recurrenceOrder.getNextPurchase();
+        this.setPurchaseOrder(PurchaseRequestDTO.convert(recurrenceOrder.getPurchaseOrder()));
     }
 }
