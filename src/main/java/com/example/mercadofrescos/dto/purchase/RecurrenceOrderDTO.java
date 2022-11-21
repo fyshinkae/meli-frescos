3ac26297c9bf77e7785b21e6767d61b03295b249
Anderson Alves <anderson.malves@mercadolivre.com>
2022-11-21 17:58:24 -0300

    Add: list recurrency with purchaseOrder

--- src/main/java/com/example/mercadofrescos/dto/purchase/RecurrenceOrderDTO.java
+++ src/main/java/com/example/mercadofrescos/dto/purchase/RecurrenceOrderDTO.java
@@ -1,5 +1,6 @@
 package com.example.mercadofrescos.dto.purchase;
 
+import com.example.mercadofrescos.model.PurchaseOrder;
 import com.example.mercadofrescos.model.RecurrenceOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,10 +14,11 @@ import java.time.LocalDate;
 public class RecurrenceOrderDTO {
     private Long orderId;
     private LocalDate nextPurchase;
+    private PurchaseRequestDTO purchaseOrder;
 
     public RecurrenceOrderDTO(RecurrenceOrder recurrenceOrder) {
         this.setOrderId(recurrenceOrder.getId());
         this.setNextPurchase(recurrenceOrder.getNextPurchase());
+        this.setPurchaseOrder(PurchaseRequestDTO.convert(recurrenceOrder.getPurchaseOrder()));
     }
-    // private LocalDate nextPurchase;
 }
